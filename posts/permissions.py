4cbc342f522bfdea3136145fff6f2dc3130c107f
muralievv <nmuraliev@gmail.com>
2026-02-27 16:57:02 +0600

    hm2

--- posts/permissions.py
+++ posts/permissions.py
@@ -5,3 +5,10 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.author == request.user
+
+
+class IsModerator(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated and request.user.is_staff and request.method != 'POST':
+            return True
+        return False
--- users/models.py
+++ users/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from .managers import UserManager
 
 class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(unique=True) # ПУНКТ: email для регистрации
-    phone_number = models.CharField(max_length=15, blank=True, null=True) # ПУНКТ: телефон
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True) 
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
